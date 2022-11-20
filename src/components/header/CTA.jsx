fd688c0c79f92416b064bf5a55ccc281fdb83dbc
Tomas Makaj <Tmakajcoding@gmail.com>
2022-11-19 20:22:36 -0500

    contact me

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -6,7 +6,7 @@ const CTA = () => {
   return (
     <div className='cta'>
         <a href= {Resume} target = "_blank " className='btn'>Resume</a>
-        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        <a href="#contact" className='btn btn-primary'>Contact Me</a>
     </div>
   )
 }
